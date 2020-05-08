d041de5614fa91156688c2a674544339c2673eff
wspfilho <weddigitalsolutions@users.noreply.github.com>
2020-05-08 19:46:41 -0300

    Avatar, Card, UserItem and UserList components

---
+++ src/user/components/UserList.component.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+import "./UserList.styles.css";
+
+import UserItem from "./UserItem.component";
+
+const UsersList = (props) => {
+  if (props.items.length === 0) {
+    return (
+      <div className="center">
+        <h2>No users found.</h2>
+      </div>
+    );
+  }
+
+  return (
+    <ul className="users-list">
+      {props.items.map((user) => (
+        <UserItem
+          key={user.id}
+          id={user.id}
+          image={user.image}
+          name={user.name}
+          placeCount={user.places}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default UsersList;
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,16 @@
 import React from "react";
+import UserList from "../components/UserList.component";
 
 const Users = () => {
-  return <h1>Users Page</h1>;
+  const USERS = [
+    {
+      id: "id",
+      name: "Wilson Filho",
+      image: "https://source.unsplash.com/1600x900/?nature,water",
+      places: 3,
+    },
+  ];
+  return <UserList items={USERS} />;
 };
 
 export default Users;
