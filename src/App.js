624170a48140e18ad67b252449501100c75765f2
wspfilho <weddigitalsolutions@users.noreply.github.com>
2020-05-08 17:20:26 -0300

    Starting setup, pages and routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
 
-function App() {
+import Users from "./user/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Users} />
+        <Route path="/places/new" component={NewPlace} />
+        <Redirect to="/" />
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/places/pages/NewPlace.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NewPlace = () => {
+  return <h1>NewPlace Page</h1>;
+};
+
+export default NewPlace;
